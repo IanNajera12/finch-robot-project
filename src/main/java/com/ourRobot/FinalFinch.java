1f96029015abb71e3474fab6dcbe067201c222d3
IanNajera12 <inajera0010@mymail.lausd.net>
2025-06-02 15:35:14 -0700

    updated FinalFinch

---
+++ src/main/java/com/ourRobot/FinalFinch.java
@@ -0,0 +1,20 @@
+package com.ourRobot;
+
+import com.birdbrain.Finch;
+
+public class FinalFinch {
+    public static void main(String[] args) {
+        Finch bird = new Finch();
+
+
+        // DO STUFF HERE!
+        bird.setMove("F", 5, 100);
+
+
+
+
+
+        bird.stopAll();
+        bird.disconnect();
+    }
+}
